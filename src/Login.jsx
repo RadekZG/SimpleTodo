1f4d53882ecd046d6da3222091ed3b253868e101
Radek Pulkrabek <203909464+RadekZG@users.noreply.github.com>
2025-09-21 19:15:45 +0200

    working auth

---
+++ src/Login.jsx
@@ -0,0 +1,32 @@
+import './style.css'
+import { useState, useEffect } from 'react'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import supabase from "./helper/supabaseClient";
+import App from './App';
+
+
+export default function Login() {
+    const [session, setSession] = useState(null)
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+        setSession(session)
+        })
+
+        const {
+        data: { subscription },
+        } = supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session)
+        })
+
+        return () => subscription.unsubscribe()
+    }, [])
+
+    if (!session) {
+        return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+    }
+    else {
+        return <App />
+    }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
-import App from './App.jsx'
+import Login from './Login.jsx'  // <-- use Login here
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
 )
